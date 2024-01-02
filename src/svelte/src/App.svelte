7217ef0245667f08dd0e1d9d72fd52d6c60abe49
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-01-02 10:23:42 +0800

    feat: svelte history router

--- src/svelte/src/App.svelte
+++ src/svelte/src/App.svelte
@@ -1,10 +1,18 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import { routes } from './router';
+  import { Router, Route } from 'svelte-routing';
+  import OnePage from './pages/One.svelte';
+  import TwoPage from './pages/Two.svelte';
+  import ThreePage from './pages/Three.svelte';
+
+  export let url = ""
 </script>
 
 <main>
-  <Router {routes} />
+  <Router {url} basepath={window.__MICRO_APP_BASE_ROUTE__}>
+    <Route path="/one" component={OnePage} />
+    <Route path="/two" component={TwoPage} />
+    <Route path="/three" component={ThreePage} />
+  </Router>
 </main>
 
 <style>
